35fc2be53bf8315389271b357dc45d0e0189adcb
Laia Rodriguez Ramos <lrodrigu22@jaumebalmes.net>
2023-12-01 17:08:39 +0100

    Commit 01/12

---
+++ LRR_Arrays.js
@@ -0,0 +1,17 @@
+/*
+
+Crea una funció JavaScript que crei un array de 6 colors per nom (‘verd’, ‘vermell’, ‘groc’, ‘blau’,
+‘negre’, ‘blanc’) i que:
+
+ digui si tots els colors són menors que ‘marró’
+ et torni els colors que són menors que ‘marró’
+ et torni els darrers 2 colors (independentment de quants hi hagin abans)
+ afegeixi el color ‘turquesa’
+ elimini el color ‘verd’
+(ha d’escriure en una pàgina Web)
+
+*/
+
+function lrr_Colors(){
+    let array = ['verd', 'vermell', 'groc', 'blau', 'negre', 'blanc']
+}
